01da4bd08f1554be7ff8dbb5184c99cc5315d5c2
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-22 17:29:10 +0530

    Javascript

---
+++ Day-2/index.js
@@ -0,0 +1,68 @@
+
+
+// console.log(1)
+// console.log(2);
+// setTimeout(()=>{
+//     console.log(3);
+    
+// })
+// console.log(4);
+
+
+/**
+ * 
+ * variables in Javascript:-
+ * ---------------------------
+ * Variables are acts as a container, it is used to store the datatypes(Values)
+ * 
+ * diff types of variables:-
+ * ---------------------------
+ * 
+ * var
+ * let
+ * const
+ * automatic
+ * 
+ * 
+ * sy:-
+ * 
+ * 
+ * keyword   identifier=value;
+ * 
+ * var/let/const   name="Hello"
+ * 
+ * 
+ */
+
+// var a=10;
+// a=20;
+// console.log(a)
+
+
+// var Name="javascript";
+// console.log(name);
+
+
+
+// var x="Rajesh";
+// console.log(name)//space
+// console.log(Name)//un, javasc, not
+// console.log(x)//rajesh
+
+// var _123=123;
+// console.log(_123);//error, 123
+
+// var abc="javascript";
+// console.log(abC);//not
+
+
+// var _x123ab="hello";
+// console.log(_x123ab);
+
+
+
+// const const=20;
+// console.log(const);
+
+
+
---
+++ Day-3/index.js
@@ -0,0 +1,102 @@
+/**
+ * 
+ * hoisting :-
+ * ---------
+ * hoisting is a process of accessing the variables before intilization or assignment
+ * 
+ * 
+ * 
+ */
+
+// console.log(a);
+// var a=10;
+// console.log(a)
+
+//GEC:-
+/**
+ * GEC stands for Global exicution context
+ * 
+ * GEC is the starting and ending point of our javascript code
+ * 
+ * the code written inside the function, it will take care by the FEC.
+ * 
+ * the function Outside code will take care by the GEC
+ * 
+ * 
+ * memory Phase, Exicution Phase
+ * 
+ * 
+ */
+
+
+// var a=34;
+// var a;
+// var a=43;
+// var b=20;
+// console.log(a);
+// console.log(b)
+// console.log(c)
+
+
+// console.log(a);
+// var a=10;
+// console.log(b);
+// var b=20;
+// console.log(a);
+// console.log(c);
+// var c=30;
+// console.log(b);
+// console.log(c)
+
+
+// console.log(a,b,c,d)//unde
+// var a=100;
+// console.log(a);//100
+// var b=200;
+// console.log(b);//200
+// var c=100+b;
+// console.log(c);//300
+// var d=300;
+// console.log(d)//300
+
+
+// datatypes in Js:-
+// -------------------------
+
+
+// Primitive and Non Primitive
+/**
+ * Primitive Datatypes:-
+ * ------------------------
+ * primitive Datattypes are immutable(once you are assining a value we can not change )
+ * 
+ * ->Number
+ * ->string
+ * ->Boolean
+ * ->Undefined
+ * ->null
+ * ->Symbol
+ * 
+ * 
+ * Non-Primitive datatypes:-
+ * -----------------------------
+ * Non_primitive datatypes are muttable(once you are assigning a value, we have to modfie)
+ * 
+ * 
+ * ->Arrays
+ * ->Objects
+ * ->set
+ * ->functions
+ * etc.,
+ * 
+ * 
+ */
+
+
+
+// var a=null;
+// console.log(typeof(a))
+
+
+
+
