2e51517c9e6b043c4c5d290a92c2aac5a037b533
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-23 17:43:51 +0530

    Day-2 operators

---
+++ Day-4/index.js
@@ -0,0 +1,109 @@
+/**
+ * 
+ * Operators:
+ * -------------
+ * Arthematic operators:
+ * ------------------------
+ * Addition(+)
+ * Subtraction(-)
+ * Multiplication(*)
+ * Division(/)
+ * Exponentioation(**)
+ * Modulus(%)
+ * Incriment(++)
+ * Decrement(--)
+ * 
+ * 
+ * Asssignment operators:
+ * ------------------------
+ * =
+ * +=
+ * -=
+ * *=
+ * **=
+ * /=
+ * %=
+ * 
+ * var score=99;
+ *score +=1;//score =score +1;
+ * console.log(score);
+ 
+ * 
+ * 
+ * comparison operators:
+ * ----------------------
+ * ==
+ * ===
+ * !=
+ * !==
+ * <=
+ * >=
+ * <
+ * >
+ * 
+ * logical operators:
+ * -------------------
+ * logical AND(&&)
+ * logical OR(||)
+ * 
+ * Terenary Operators:
+ * -------------------
+ * condition? expressionisTrue : expressionisFalse;
+ * 
+ */
+// let a=2;
+// a+=2;//a=a+2
+// a-=2;//a=a-2
+// a*=2;
+// let b=3;
+// console.log(a+b);//3
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a++);
+// console.log(b--);
+
+// let age=20;
+// let a=age>=18? "Adult" : "Minor";
+// console.log(a);
+
+// var age="18";
+// console.log(age === 18)
+
+// let score=prompt("Enter score")
+// console.log(score-10);
+
+// explacit type casting
+
+// Number()
+// Boolean()
+
+// console.log(true+true);//2
+// console.log(true+false);//1
+// console.log(false+true);//1
+// console.log(false+false);//0
+
+// var a=Boolean(0);
+// var b=true
+// console.log(a+b);
+
+
+// var score=prompt("enter score")
+// console.log(score+10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
