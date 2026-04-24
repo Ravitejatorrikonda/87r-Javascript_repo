e6bd428634a81e93ac7b5db2571269d8a6ff6c52
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-24 17:26:38 +0530

    Conditional Statements

---
+++ Day-5/index.js
@@ -0,0 +1,102 @@
+/**
+ * 
+ * conditional statements
+ * ------------------------
+ * if statements
+ * 
+ * syntax
+ * 
+ * if(condition){
+ *  //block of code(true)
+ * }
+ * 
+ * 
+ * else-if statement
+ * 
+ * if(condition){
+ * //block of code(true)
+ * 
+ * }
+ * else{
+ * //block of code(false)
+ * }
+ * 
+ * else-if ladder
+ * 
+ * if(condition){
+ * 
+ * }else if(condition){
+ * 
+ * }else{
+ * 
+ * }
+ * 
+ * switch statement
+ * 
+ * switch(expression){
+ *      case1:
+ *          break;
+ * 
+ *      case2:
+ *          break;
+ * 
+ *      default:
+ * 
+ * 
+ * }
+ * 
+ * 
+ */
+
+ 
+
+
+
+
+// let orderAmount=600;
+// if(orderAmount>=500){
+//     console.log("free delivery charges:")
+// }
+
+let amount=1000;
+let withdraw=1200;
+//if(amount>=withdraw){
+   // console.log("Transaction Completed");
+//}else{
+  //  console.log("Insufficient balance");
+//}
+
+if(amount>=withdraw){
+    console.log("Transaction completed");
+}
+if(amount<=withdraw){
+    console.log("Insufficinet balance");
+}
+
+
+// let age=32;
+
+// if(age<12){
+//     console.log("Child ticket");
+// }else if(age>=12&&age<50){
+//     console.log("Adult ticket");
+// }else{
+//     console.log("Cinear citizen ticket");
+// }
+
+
+// let day=2;
+// switch(day){
+//     case 1:
+//         console.log("sunday:");
+//         break;
+//     case 2:
+//         console.log("monday:");
+//         break;
+//     case 2:
+//         console.log("tuesday:");
+//         break;
+
+//     default:
+//         console.log("invalid day");
+// }
