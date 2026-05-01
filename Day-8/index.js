e675be4ce3a2801ce3b21eccce9819dfd2dc74ec
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-05-01 17:19:11 +0530

    FUn-2\

---
+++ Day-8/index.js
@@ -0,0 +1,58 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+// function print(){
+//     console.log("hello There");
+
+//     return "Hi";
+
+// }
+// var result=print();
+// console.log(result)
+
+// function x(a,b){
+//     return a+b;
+// }
+// console.log(x());
+
+
+
+// var x=10;
+// function print(){
+//     var x=20;
+//     console.log(x);
+
+// }
+// print();
+
+// var a;
+// function x(){
+//     var a=10;
+//     console.log(a);
+
+// }
+// x();
+// console.log(a);
+
+// function x(){
+//     return a;
+//     var a=20;
+// }
+// console.log(x());
+
+
+function pushpa(hero, villan) {
+    console.log(hero + villan);
+    devara(hero, villan)
+    return devara("NTR","prakshraj")
+
+}
+function devara(hero, char) {
+    console.log(hero+char)
+
+}
+console.log(pushpa("allu","PK"));
