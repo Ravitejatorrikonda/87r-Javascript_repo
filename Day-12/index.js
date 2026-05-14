58164ce5c0c6ed57c86ded203b7087802c89f69e
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-05-14 16:15:20 +0530

    Scopes

---
+++ Day-12/index.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// if(true){
+//     const a=43;
+//     console.log(a)
+// }
+// function x(){
+    
+   
+//     console.log(a)
+// }
+// x()
+
+
+
+// console.log(a)
+
+
+
+const a=10;
+const a;
+console.log(a)
