5cb6c7abcc66f37fb7ff49fe88d3d79c7049cf9c
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-06-30 17:41:12 +0530

    DOM-1

---
+++ Day-22/index.js
@@ -0,0 +1,52 @@
+/**
+ * asyn and await
+ * ---------------
+ * 
+ * Error handling:-
+ * -----------------
+ * 
+ * 
+ */
+
+// function printData(a, b) {
+//     try {
+//         if(b==0){
+//             throw new Error("Not Possibble");
+//         }else{
+//             let x=a/b;
+//             console.log(x)
+//         }
+//     } catch (error) {
+//         console.error(error.message)
+//     }
+// }
+// printData(100, 0)
+// console.log("completed..")
+
+
+
+
+// async function fetchData() {
+//     let res=await fetch("http://localhost:3000/dta")
+//     try {
+//         if(!res.ok){
+//             throw new Error(`Fetching has an issue ${res.status}` )
+//         }
+//         let data=await res.json();
+//         console.log(data)
+//     } catch (error) {
+//         console.error(error.message)
+//     }
+// }
+// fetchData();
+// console.log("Completed")
+
+
+/**
+ * 
+ * 
+ * DOM:-(DOCUMENT OBJECT MODEL)
+ * ------
+ * 
+ */
+
