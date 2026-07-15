bd86aac15240533782a890a0985bdccebef671bf
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-07-15 16:36:18 +0530

    Clousers

---
+++ Day-28/index.js
@@ -0,0 +1,70 @@
+/**
+ * 
+ * 
+ * What is JS?
+ * V8 Engine
+ * GEC
+ * Variables
+ * datatypes
+ * operators
+ * conditional statements
+ * loops
+ * FEC
+ * Functions
+ *    named, anonymous, arrow, callback,first class, higher order
+ * Scopes
+ * callbackhell
+ * callstack,
+ * promises
+ * fetch, get,put,post,patch
+ * arrsy methods, iterative methods, try catch, asyn and await, DOM
+ * 
+ * clousers, and lexical scope
+ * BOM
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+// function x(){
+//     let a=10;
+//    return  function y(){
+//         console.log(a)
+//     }
+ 
+
+// }
+// let a=x();
+// a();
+
+
+// for(let i=1; i<=5;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1000)
+// }
+
+
+
+// function addtocart(intialcount){
+//     let count=intialcount;
+//     return (totalcount)=>{
+//         count +=totalcount
+//         console.log(count)
+//     }
+// }
+
+// let cart=addtocart(1)
+// cart(0);
+
+
+// let reg=/Hello/;
+// console.log(reg.test("Hello"))
+
+//BOM(Browser object model)
+
+
+// console.log(localStorage.getItem("Name"))
