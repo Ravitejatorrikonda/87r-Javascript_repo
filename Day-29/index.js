3387fb4bc0e34bf45f09b68681d03e0b40de6f95
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-07-16 16:34:43 +0530

    BOM-1

---
+++ Day-29/index.js
@@ -0,0 +1,69 @@
+/**
+ * 
+ * BOM:-(Browser Object Model):-
+ * -------------------------------
+ * 
+ * 
+ * 
+ */
+// let resultwindow;
+// function openWindow() {
+//     resultwindow= window.open("", "", "width=500px,height=500px");
+//     // resultwindow.document.write(`
+//     //     <!DOCTYPE html>
+//     //     <html><head>
+//     //     <style>
+//     //     h3{
+//     //     color:blue;
+//     //     }
+//     //     </style>
+//     //     </head>
+//     //     <body>
+//     //     <h3>Hello There</h3>
+//     //     </body>
+
+        
+//     //     </html>
+        
+//     //     `)
+
+
+
+
+
+//     // window.setTimeout(() => {
+//     //   resultwindow.close();
+//     // }, 5000)
+
+
+
+// }
+
+// let flag=null;
+// function closedata(){
+//     let flag=window.confirm("Do you want close?",resultwindow);
+//     if(flag){
+//         resultwindow.close();
+//     }else{
+//         window.alert("Thank you.....")
+//     }
+// }
+
+
+// window.setInterval(()=>{
+// window.location.reload();
+// },1000)
+
+// let result=null;
+// function startdata(){
+
+//    let count=0;
+//    result= window.setInterval(()=>{
+//         let date=new Date();
+//         console.log(`Sdeconds : ${date.getSeconds()}`)
+//     },1000)
+
+// }
+// function stopdata(){
+//      window.clearInterval(result);
+// }
